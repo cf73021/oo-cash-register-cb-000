999775cc52a4c1e857746b84c7dc6078bc48b926
Chris Franklin <cf73021@gmail.com>
2018-08-08 20:45:44 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
 
   def apply_discount
     @discount = @discount.to_f
-    @discount = @discount / 1000
+    @discount = @discount / 100
     @total = @total.to_f
     @subtraction = @total * @discount
     @total = @total - @subtraction
